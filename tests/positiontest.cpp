0cc67f3faf6b33210357e6758082a0d650eec507
Carmelo908 <carmelopepigauna@gmail.com>
2026-05-02 15:57:09 -0300

    Expanded positiontest and added a benchmark to measure Position::advanceGen() time

--- tests/positiontest.cpp
+++ tests/positiontest.cpp
@@ -1,9 +1,14 @@
 #define CATCH_CONFIG_MAIN
+#include <filesystem>
 #include <string>
 
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <nlohmann/json.hpp>
 
 #include <position.hpp>
+
+auto benchmarkFilename = "benchmark_position.json";
 
 std::string posToString(Position &pos)
 {
@@ -19,7 +24,23 @@ std::string posToString(Position &pos)
   return posString;
 }
 
-TEST_CASE("Testing Position functionality")
+TEST_CASE("Opening a position from json")
+{
+  auto json = nlohmann::json::parse(R"([
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+  ])");
+  Position pos = Position::parseJson(json);
+  Position expected{{
+      {0, 1, 0},
+      {1, 1, 1},
+      {0, 1, 0},
+  }};
+  REQUIRE(pos == expected);
+}
+
+TEST_CASE("Position functionality")
 {
   SECTION("Comparision operator")
   {
@@ -30,7 +51,7 @@ TEST_CASE("Testing Position functionality")
     CHECK(pos1 == pos2);
   }
 
-  SECTION("Advance generation")
+  SECTION("Advance generation", "[!benchmark]")
   {
     Position pos{{
         {0, 1, 0},
@@ -44,6 +65,23 @@ TEST_CASE("Testing Position functionality")
         {0, 1, 0},
     }};
     INFO(posToString(pos));
-    CHECK(pos == nextPos);
+    REQUIRE(pos == nextPos);
+    BENCHMARK_ADVANCED("Advance 50x50 generation 100 times")
+    (Catch::Benchmark::Chronometer meter)
+    {
+      auto benchmarkFilepath =
+          std::filesystem::path(__FILE__).remove_filename().append(
+              benchmarkFilename);
+      Position benchmarkPosition{Position::parseJsonFile(benchmarkFilepath)};
+      auto advance100Times = [&]() {
+        for (int i = 0; i < 100; i++)
+        {
+          benchmarkPosition.advanceGen();
+        }
+      };
+      meter.measure([&]() {
+        return advance100Times();
+      });
+    };
   }
 }
